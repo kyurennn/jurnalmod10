21649dc732e45377bc7d870c5d400287d098718f
HCarrr <huda012354@gmail.com>
2026-05-05 07:42:08 +0700

    Menambahkan API GAme

---
+++ Modul10_103022400056/Controllers/GameController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Modul10_103022400056;
+using Modul10_103022400056.Controllers;
+
+namespace Modul10_103022400056.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GameController : ControllerBase
+    {
+        private static List<Game> Games = new List<Game>
+        {
+            new Game("Valorant", "Riot Games", "2020", "FPS", 8.5, ["PC"], ["Multiplayer"], true, 0),
+            new Game("GTA V", "Rockstar Games", "2013", "Open World", 9.5, ["PC", "PS4", "PS5", "XBOX"], ["Singleplayer, Multiplayer"], true, 300000),
+            new Game("The Witcher 3", "CD Projekt Red", "2015", "RPG", 9.7, ["PC", "PS4", "PS5", "XBOX", "Switch"], ["Singleplayer"], false, 25000)
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public GameController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<Game> Get()
+        {
+            return Games;
+        }
+
+        [HttpGet("{id}")]
+        public Game GetById([FromRoute] int id)
+        {
+            return Games[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Game newGame)
+        {
+            Games.Add(newGame);
+        }
+
+        [HttpPut("{id}")]
+        public void Put([FromRoute] int id, [FromBody] Game updatedGame)
+        {
+            Games[id] = updatedGame;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete([FromRoute] int id)
+        {
+            Games.RemoveAt(id);
+        }
+    }
+}
---
+++ Modul10_103022400056/Game.cs
@@ -0,0 +1,28 @@
+﻿namespace Modul10_103022400056
+{
+    public class Game
+    {
+        public string Nama { get; set; }
+        public string Developer { get; set; }
+        public string TahunRilis { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+        public string[] Platform { get; set; }
+        public string[] Mode { get; set; }
+        public bool IsOnline { get; set; }
+        public int Harga { get; set; }
+
+        public Game(string nama, string developer, string tahunRilis, string genre, double rating, string[] platform, string[] mode, bool isOnline, int harga)
+        {
+            this.Nama = nama;
+            this.Developer = developer;
+            this.TahunRilis = tahunRilis;
+            this.Genre = genre;
+            this.Rating = rating;
+            this.Platform = platform;
+            this.Mode = mode;
+            this.IsOnline = isOnline;
+            this.Harga = harga;
+        }
+    }
+}
